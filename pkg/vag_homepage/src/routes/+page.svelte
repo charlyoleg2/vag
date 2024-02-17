f8603261eaf60c3a65d86efd35198782fc1e115d
charles <charles.braquet@e.email>
2024-02-17 20:16:20 +0100

    api doc link

--- pkg/vag_homepage/src/routes/+page.svelte
+++ pkg/vag_homepage/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a href="{base}/apidoc/" rel="external">API docs</a></p>

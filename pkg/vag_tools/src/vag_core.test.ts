2ecf274ebbc462d4d1c4f9767bdf895076c8fc49
charlyoleg <charles.braquet@gmx.fr>
2024-08-17 10:44:20 +0200

    add dummy vitest test for vag_tools

---
+++ pkg/vag_tools/src/vag_core.test.ts
@@ -0,0 +1,10 @@
+// vag_core.test.ts
+
+import { describe, it, expect } from 'vitest';
+//import { Vag } from './vag_core';
+
+describe('vag_core suit', () => {
+	it('vag_core test execution', () => {
+		expect(2 + 2).toBe(4);
+	});
+});
